5b8404af6afb4d23fe4db05f943f7d13c4e9a776
Ben Keener <keener4christ@gmail.com>
2020-02-13 20:57:06 +0000

    Started dict method

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+"""Module containing the base class"""
 import uuid
 import datetime
 
-class BaseModel:
+class BaseModel():
+    """Base class for all future classes"""
     def __init__(self):
         self.id = str(uuid.uuid4())
 
@@ -11,3 +12,9 @@ class BaseModel:
         """String method to return prettier version of the BaseModel"""
         return ("[{}] ({}) <{}>".format(type(self).__name__, self.id,
                                         self.__dict__))
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__"""
+        ret = self.__dict__
+
+        ret["__class__"] = type(self).__name__
