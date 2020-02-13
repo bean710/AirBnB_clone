9b9305ac0d9cf7fddb239f5664e1b12be9eb2e85
RaudoR <rivaspaulino@outlook.com>
2020-02-13 20:57:12 +0000

    added created_at and updated_at to base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
+        '''Assign the current datetime when an instance is created'''
+        self.create_at = datetime.now()
+        '''Give the current datetime when an object is changed'''
+        self.update_at = datetime.now()
 
     def __str__(self):
         """String method to return prettier version of the BaseModel"""
