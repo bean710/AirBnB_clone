0b6f043d992104040e7c5b2f36c6c8d755be9c4d
Ben Keener <keener4christ@gmail.com>
2020-02-13 21:11:27 +0000

    Completed to_dict function

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,13 @@ from datetime import datetime
 
 class BaseModel():
     """Base class for all future classes"""
+
     def __init__(self):
         self.id = str(uuid.uuid4())
         '''Assign the current datetime when an instance is created'''
-        self.create_at = datetime.now()
+        self.created_at = datetime.now()
         '''Give the current datetime when an object is changed'''
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """String method to return prettier version of the BaseModel"""
@@ -19,6 +20,10 @@ class BaseModel():
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
-        ret = self.__dict__
+        ret = self.__dict__.copy()
 
         ret["__class__"] = type(self).__name__
+        ret["created_at"] = ret["created_at"].isoformat()
+        ret["updated_at"] = ret["updated_at"].isoformat()
+
+        return ret
