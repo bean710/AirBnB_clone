adb8544511fec4fa70fd76fe5a3ea2cda9ae4aa5
Ben Keener <keener4christ@gmail.com>
2020-02-13 17:46:58 +0000

    Added some tests for BaseModeL

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for testing the Base class"""
+import unittest
+from models.base_model import BaseModel
+from datetime import date
+
+class TestBaseClass(unittest.TestCase):
+    """Contains functions for testing aspects of the base class"""
+
+    def testId(self):
+        """Tests that a unique ID is assigned to each BaseModel"""
+        bma = BaseModel()
+        bmb = BaseModel()
+
+        self.assertNotEqual(bma.id, bmb.id)
+
+    def testCreationTime(self):
+        """Tests that the creation time of a BaseModel is correct"""
+
+    def testUpdateTime(self):
+        """Tests that the update time of a BaseModel is correct"""
+
+    def testStringify(self):
+        """Tests that the str method for a BaseModel"""
