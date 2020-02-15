1645841687bc4b989046db30139ca36be0943187
RaudoR <rivaspaulino@outlook.com>
2020-02-15 00:03:57 +0000

    Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""This is the amenities class"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Public attributes for Amenity"""
+    name = ""
