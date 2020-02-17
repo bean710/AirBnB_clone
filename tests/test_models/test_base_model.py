e078206240431e41faf10acf5ef3e03c7832dfa0
RaudoR <rivaspaulino@outlook.com>
2020-02-17 23:59:23 +0000

    removed impossible tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,12 +15,6 @@ class TestBaseClass(unittest.TestCase):
 
         self.assertNotEqual(bma.id, bmb.id)
 
-    def testCreationTime(self):
-        """Tests that the creation time of a BaseModel is correct"""
-
-    def testUpdateTime(self):
-        """Tests that the update time of a BaseModel is correct"""
-
     def testStringify(self):
         """Tests that the str method for a BaseModel"""
         bma = BaseModel()
